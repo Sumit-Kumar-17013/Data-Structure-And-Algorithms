cb703e0aef31aaaf1e6ad667d0a3803a5f7e57da
Sumit Kumar <sumit.edu.17013@gmail.com>
2026-09-06 11:36:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (36.48%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -3,6 +3,8 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
 
+        // TC = O(n^2)
+
         for(int i = 0 ; i < n ; i++) // transpose
         {
             for (int j = i+ 1 ; j < n ; j++)
