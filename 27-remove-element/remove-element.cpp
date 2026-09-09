6a372764e156ce83412989d731c812957859f834
Sumit Kumar <sumit.edu.17013@gmail.com>
2026-09-09 12:09:17 +0530

    Time: 3 ms (0.99%) | Memory: 11.7 MB (84.30%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int k = 0;
+        int k = 0; // tc = O(n) 
         for(int i = 0 ; i < nums.size() ; i++)
         {
             if(nums[i] !=  val)
