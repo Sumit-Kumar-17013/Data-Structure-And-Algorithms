946880d2c3c754cd2f874f6f4ca14a3a79946abd
Sumit Kumar <sumit.edu.17013@gmail.com>
2026-09-13 22:36:58 +0530

    Time: 0 ms (100.00%) | Memory: 35.8 MB (54.55%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int Rightsum = 0;
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            Rightsum += nums[i];
+        }
+
+        int Leftsum = 0;
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(Leftsum == Rightsum - nums[i])
+            {
+                return i;
+            }
+
+            Leftsum += nums[i];
+            Rightsum -= nums[i];
+        }
+
+        return -1;
+    }
+};
