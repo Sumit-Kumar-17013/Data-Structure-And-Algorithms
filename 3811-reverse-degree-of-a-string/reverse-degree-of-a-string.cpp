081c915e456e835e01ac62abc4cd8d01d0dd3e02
Sumit Kumar <sumit.edu.17013@gmail.com>
2026-09-20 21:24:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (15.78%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans = 0;
+
+        for(int i = 0 ; i < s.size() ; i++)
+        {
+            int normalpos = s[i] - 'a' + 1;
+            int reversepos = 27 - normalpos;
+
+            ans += reversepos * (i  + 1);
+        }
+
+        return ans;
+    }
+};
