d40378cc29946c52c43618fc442c16a31496dfa8
Sumit Kumar <sumit.edu.17013@gmail.com>
2026-09-20 21:57:50 +0530

    Time: 3 ms (29.76%) | Memory: 8.4 MB (97.97%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int reverse(int num) {
+        // TC
+        int rev = 0;
+
+        while(num != 0)
+        {
+            int digit = num % 10;
+
+            if (rev > INT_MAX / 10 ||  (rev == INT_MAX / 10 && digit > 7)) 
+            {
+                return 0;
+            }
+
+
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8))
+            {
+                return 0;
+            }
+
+
+            rev = rev * 10 + digit;
+            num = num / 10;
+        }
+
+        return rev;
+    }
+};
