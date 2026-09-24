f93261573b46ee5fab1143b4f783118d2f183cdf
Sumit Kumar <sumit.edu.17013@gmail.com>
2026-09-24 21:59:11 +0530

    Time: 4 ms (33.64%) | Memory: 9.6 MB (91.92%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> freq(26 , 0);
+
+        for(char c : s)
+        {
+            int idx = c - 'a';
+            freq[idx]++;
+        }
+
+        for(char c : t)
+        {
+            int idx = c - 'a';
+            freq[idx]--;
+        }
+
+        for(int i = 0 ; i < 26 ; i++)
+        {
+            if(freq[i] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
